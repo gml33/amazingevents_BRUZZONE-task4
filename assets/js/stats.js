138bff7eadf4bd7d2d1aa766fc1b4594ecfa2064
Bruzzone Marcelo Daniel <bruzzone1804@gmail.com>
2023-03-19 18:04:46 -0300

    listos montos

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -13,13 +13,9 @@ async function getData(){
         data = await response.json();        
         let upcomingEvents = filtraPorFechaFuturo(data);
         let pastEvents = filtraPorFechaPasado(data);
-        console.log(pastEvents);
-        let upcomingCategories = obtenerCategorias(upcomingEvents);
-        let pastCategories = obtenerCategorias(pastEvents)
-        //let ganancia1 = obtenerGananciaPorCategoria(upcomingEvents, upcomingCategories[0])
-        let ganancia2 = obtenerGananciaPorCategoria(pastEvents, pastCategories[1])
-        //console.log('ganancia upcoming: '+ganancia1);
-        console.log('ganancia past: '+ganancia2);
+        //hasta aca bien, filtra los eventos por upcoming y past
+        let ganancias=eventosFiltradosPorCategorias(upcomingEvents)
+
     } catch (error) {
         console.log(error);
     }
@@ -27,16 +23,44 @@ async function getData(){
 
 getData();
 
-//upcomingEvents>categories>revenues>percentajeOfAssistance
-let obtenerCategorias =(array)=>{
+let eventosFiltradosPorCategorias=(arrayEventos)=>{
+    let values = {};
     let categorias = [];
-    array.forEach((evento)=>{
+    let i = 0;
+    let j=0;
+    let monto = 0;
+    //itero sobre el array de eventos para obtener las categorias
+    arrayEventos.forEach((evento)=>{
         let categoriaEvento = evento.category;
-        categorias.push(categoriaEvento);        
+        categorias.push(categoriaEvento);
     });
+    //hago un set sobre el resultado, para eliminar duplicados
     categorias = new Set(categorias)
+    //transformo un set en un array
     categorias = [...categorias]
-    return categorias;
+    //itero sobre el array de las categorias
+    for(i=0;i<categorias.length;i++){
+        //inicializo la variable del monto        
+        monto= 0;
+        //itero sobre el array de eventos
+        for(j=0;j<arrayEventos.length;j++){
+            //si el category del evento es igual a la categoriia del primer loop, agrgo el valor del monto multiplicado por la asistencia
+            //pero los malditos eventos no estan normalizados, por lo que a veces vienen sin todos los datos, principalmente de asistencia
+            //asi que le voy a meter un puto if aca para comprobar que vengan bien, porque despues de casi 2 dias de que esta mierda falle, me di cuenta que al no estar 
+            //normalizados los datos, me arrojaba un valor nulo cuando no detectaba la asistencia. LPM
+            //casi prendo fuego mi computadora de la bronca que me agarró, asi que antes de seguir, me voy a tomar un buen tereré y mientras me tranquilizo mirando al al 
+            //cielo, voy a regar mis plantas, me voy a bañar y quizas después siga.......
+            if(arrayEventos[j].category && arrayEventos[j].price && arrayEventos[j].assistance){
+                if(categorias[i]==arrayEventos[j].category){                   
+                    monto = monto+arrayEventos[j].price*arrayEventos[j].assistance;
+                }
+            }else{
+                //fuck off
+            }            
+        }             
+        values[i] = {'categoria':categorias[i],'monto':monto};
+    }
+    console.log(values);
 }
 
 let obtenerEventosPorCategoria = (eventos, categorias)=>{
