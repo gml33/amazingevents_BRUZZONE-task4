d13a5bfbb55dff2e8b5d4ce7afbffca0522475bc
Bruzzone Marcelo Daniel <bruzzone1804@gmail.com>
2023-03-22 19:17:44 -0300

    listo vue

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,64 +1,57 @@
-import { crearCards, crearCheckBoxes, createCategories, filtroArray, mensaje} from './helpers.js';
-
-let data = [];
-
-async function getData(){
-  try {
-    const apiUrl = '/assets/js/amazing.json';
-    const response = await fetch(apiUrl);
-    data = await response.json();
-    crearCheckBoxes(createCategories(data.events), containerCheckBoxes); // Imprimo los radios de categorías
-    crearCards(data.events,containerCard);//cargo por primera vez las cards
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-getData();
-
-/*agregar un eventListener a cada checkbox y obtengo su estado*/
-let datos = []
-containerCheckBoxes.addEventListener('change',()=>{
-let elementos=[]
-let listaChecked = document.querySelectorAll('input[name="category"]:checked')
-listaChecked.forEach((item)=>{
-  elementos.push(item.defaultValue)
-  })
-  if(elementos.length==0){
-    crearCards([],containerCard);
-    crearCards(data.events,containerCard);
-  }
-  else{
-    crearCards([],containerCard);
-    datos = filtroArray(data.events, elementos)
-    crearCards(datos, containerCard)      
-  }
-})
-
-/*agregar un eventListener a la busqueda asi se filtra por categoria*/
-let buscador = document.querySelector('input[placeholder="Search"]')
-buscador.addEventListener('input',()=>{
-  let datafiltrada = [];
-  let elementos=[];
-  let listaChecked = [];
-  let datos = data.events;
-  listaChecked = document.querySelectorAll('input[name="category"]:checked')  
-  listaChecked.forEach((item)=>{    
-    elementos.push(item.defaultValue)
-  })
-  if(listaChecked.length>0){
-    datos = filtroArray(data.events, elementos)
-  }
-  datos.forEach(element => {    
-    if(element.name.toLowerCase().includes(buscador.value.toLowerCase())){
-      datafiltrada.push(element);
-    }
-  })  
-  if(datafiltrada.length==0){
-    /*Mostrar tarjeta o mensaje que no hay un carajo*/
-    crearCards([],containerCard);
-    mensaje(containerCard)
-  }else{    
-    crearCards(datafiltrada,containerCard);
-  }
-});
+const { createApp } = Vue;
+const apiUrl = "../../assets/js/amazing.json";
+const app = createApp({
+    data() {
+        return {
+        eventos: [],
+        categorias: [],
+        eventosFiltrados: [],
+        valorBusqueda: "",
+        checked: [],
+        evento: undefined,
+        titulo: "",
+        };
+    },
+    created() {
+        this.getData();        
+    },
+    methods: {
+        async getData() {
+        try {
+            const response = await fetch(apiUrl);
+            let data = await response.json();
+            if (document.title.includes("Details")) {
+                let aux = location.search;
+                let params = new URLSearchParams(aux);
+                let id = params.get("id");
+                this.evento = data.find((evento) => evento.id === id);
+                this.titulo = this.evento.name;
+            } else {
+                if(document.title.includes("Upcoming")){
+                    this.eventos = data.events.filter(e=>e.estimate)
+                }
+                else if(document.title.includes("Past")){
+                    this.eventos = data.events.filter(e=>e.assistance)
+                }
+                else{
+                    this.eventos = data.events;
+                }                
+                this.eventosFiltrados=this.eventos;
+                this.categorias = [ ...new Set(this.eventos.map( e=>e.category )) ]
+            }
+            //return data;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    computed: {
+        filtro() {
+            console.log(this.checked);
+            this.eventosFiltrados = this.eventos.filter((evento) =>(
+                this.checked.includes(evento.category) || this.checked.length === 0) && evento.name.toLowerCase().includes(this.valorBusqueda.toLowerCase())
+                );
+            },
+        },
+    });
+app.mount("#app");
